69fbeaaf984731f9e55743c10f7d93cf5cb122cc
Yoki Yu <74389358+iamyoki@users.noreply.github.com>
2022-08-05 12:52:48 +0800

    feat: add list command

--- bin/begin-script.js
+++ bin/begin-script.js
@@ -4,14 +4,27 @@ import chalk from 'chalk';
 import yargs from 'yargs';
 import {hideBin} from 'yargs/helpers';
 import {defaultCommand} from '../lib/commands/defaultCommand.js';
+import {listCommand} from '../lib/commands/listCommand.js';
 import {getPkg} from '../lib/getPkg.js';
+
+process.on('uncaughtException', err => {
+  console.log(chalk.bgRed.bold(err.name))
+  console.error(chalk.red(err.message))
+  process.exit(1)
+});
 
 const pkg = getPkg();
 
 const argv = yargs(hideBin(process.argv))
   .usage(chalk.magenta('\n' + pkg.description))
-  .command('$0', 'Begin to run', {}, defaultCommand)
+  .option('config', {
+    alias: 'c',
+    desc: 'Specified config filepath.\nDefaults to ".beginrc"',
+    type: 'string'
+  })
   .example('npx $0', 'Begin to run')
-  .option('config', {alias: 'c', desc: 'Specified config filepath.\nDefaults to ".beginrc"', type: 'string'})
+  .help('help')
   .alias('help', 'h')
-  .alias('version', 'v').argv;
+  .alias('version', 'v')
+  .command('$0', 'Begin to run', {}, defaultCommand)
+  .command('list', 'Show task list', {}, listCommand).argv;
--- proposal/commands.md
+++ proposal/commands.md
@@ -10,15 +10,11 @@ Begin a user-friendly interactive command-line interface to run tasks.
 
 `yarn begin run <id|name>`
 
-|      |                                                       |                |
-| ---- | ----------------------------------------------------- | -------------- |
-| id   | Task id that defined in begin config file for task    | unique         |
-| name | Task name that defined in begin config file for task. | fuzzy matching |
+|      |                                                      |                |
+| ---- | ---------------------------------------------------- | -------------- |
+| id   | Task id that defined in begin config file for task   | unique         |
+| name | Task name that defined in begin config file for task | fuzzy matching |
 
 ## Show task list
 
 `yarn begin list`
-
-or
-
-`yarn begin --help`
