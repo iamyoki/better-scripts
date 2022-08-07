665dcd6bd31cddddea3134e8962d6eba4c8f5b54
iamyoki <yokiyuqian@gmail.com>
2022-08-07 21:05:21 +0800

    feat(dev): add schema.json generation

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,19 +1,44 @@
+/**
+ * Script array form value
+ */
+export type ScriptArrayValue = [scriptName: string, desc: string];
+
+/**
+ * Script object form value
+ */
 export type ScriptObjectValue = {
+  /**Aliase displayed in place of script name, for emoji purpose */
   alias?: string;
-  command: string;
+  /**Command to execute */
+  command?: string;
+  /**Description for the script */
   desc?: string;
-  scripts?: ScriptValue[];
+  /**Child scripts that need to be prompts after */
+  scripts?: Config;
+  /**Set environment variables for the script */
   env?: {
     [key: string]: string;
   };
 };
 
-export type ScriptValue = string | [script: string, desc: string] | ScriptObjectValue;
+/**
+ * Script value, can be either string, array or object
+ */
+export type ScriptValue = string | ScriptArrayValue | ScriptObjectValue;
 
+/**
+ * The shape of user's config file
+ */
 export interface Config {
+  /**
+   * @param name Script name
+   */
   [name: string]: ScriptValue;
 }
 
+/**
+ * The shape of internal parsed config file
+ */
 export interface ParsedConfig {
   [name: string]: ScriptObjectValue;
 }
---
+++ scripts/generate-jsonschema.js
@@ -0,0 +1,20 @@
+import chalk from 'chalk';
+import {writeFileSync} from 'node:fs';
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import * as TJS from 'typescript-json-schema';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const program = TJS.getProgramFromFiles([
+  path.resolve(__dirname, '../lib/types.d.ts')
+]);
+
+const schema = TJS.generateSchema(program, 'Config', {required: true});
+
+const filename = path.join(__dirname, '../lib', 'schema.json')
+
+writeFileSync(filename, JSON.stringify(schema, null, 2));
+
+console.log(chalk.green('📄 Schema file generated:'), chalk.bgGreen(`${filename}`))
